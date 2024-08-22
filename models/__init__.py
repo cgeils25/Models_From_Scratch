287d9eb01d3d9658e0ebce3cf35a1ef53c5fbbec
cgeils25 <cgeils@email.sc.edu>
2024-08-22 07:31:18 -0400

    extra import statements

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
 from .logistic_regression_model import LogisticRegressionModel
 from .support_vector_machine import SupportVectorMachine
+from .linear_regression import LinearRegressionModel
+from .principal_component_analysis import PCA
