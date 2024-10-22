56844f2ad47e4c4163700b0890e33cca42299419
cgeils25 <cgeils@email.sc.edu>
2024-10-22 12:01:18 -0400

    refactored logistic_regression_model to logistic_regression and fixed models __init__ import statements

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 # So I can import each of the models directly from models
-from .logistic_regression_model import LogisticRegressionModel
+from .logistic_regression import LogisticRegressionModel
 from .support_vector_machine import SupportVectorMachine
 from .linear_regression import LinearRegressionModel
 from .principal_component_analysis import PCA
 from .gaussian_naive_bayes import GaussianNaiveBayesClassifer
+from .k_nearest_neighbors import KNearestNeighborsClassifier
