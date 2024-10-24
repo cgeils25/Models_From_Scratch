6fbd98606c6ea9d707557801f53e83c8c3ac86d8
cgeils25 <cgeils@email.sc.edu>
2024-10-24 12:02:01 -0400

    unit testing for split_dataset with multiple outputs (y.ndim > 1)

--- tests/utils/test_data_utils.py
+++ tests/utils/test_data_utils.py
@@ -36,3 +36,19 @@ def test_split_dataset_seeded():
     assert np.array_equal(y_train_1, y_train_2)
     assert np.array_equal(X_test_1, X_test_2)
     assert np.array_equal(y_test_1, y_test_2)
+
+def test_split_dataset_multi_output():
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+    y = np.array([[0, 1], [1, 0], [0, 1], [1, 0], [0, 1]])
+
+    X_train, y_train, X_test, y_test = split_dataset(X, y, test_size=0.2, shuffle=False)
+
+    assert X_train.shape == (4, 2)
+    assert y_train.shape == (4, 2)
+    assert X_test.shape == (1, 2)
+    assert y_test.shape == (1, 2)
+
+    assert np.array_equal(X_train, X[:4]), f"X not split correctly into X_train, expected {X[:4]} but got {X_train}"
+    assert np.array_equal(y_train, y[:4]), f"y not split correctly into y_train, expected {y[:4]} but got {y_train}"
+    assert np.array_equal(X_test, X[4:]), f"X not split correctly into X_test, expected {X[4:]} but got {X_test}"
+    assert np.array_equal(y_test, y[4:]), f"y not split correctly into y_test, expected {y[4:]} but got {y_test}"
