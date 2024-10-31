fa9f534b83602e8d38a22da6a38a2afe30588918
cgeils25 <cgeils@email.sc.edu>
2024-10-31 08:24:19 -0400

    fixed split_dataset to work for multiple outputs, passed test

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -11,8 +11,8 @@ def _validate_X_y(X=None, y=None):
         if y is not None:
           if not isinstance(y, np.ndarray):
               raise TypeError('y must be a numpy array')
-          if len(y.shape) != 1:
-              raise ValueError(f'y must be a 1D array of shape (num_samples, ), instead got {y.shape}')
+          if y.ndim not in [1, 2]:
+              raise ValueError(f'y must be a 1D or 2D array, instead got shape {y.shape}')
 
         if y is not None and X is not None:
           if X.shape[0] != y.shape[0]:
@@ -24,7 +24,7 @@ def split_dataset(X: np.ndarray, y: np.ndarray, test_size=0.2, shuffle=True, see
 
     Args:
         X (np.ndarray): feature matrix
-        y (np.ndarray): target vector
+        y (np.ndarray): target vector or matrix
         test_size (float, optional): percentage of data to be used for testing. Defaults to 0.2.
         shuffle (bool, optional): shuffle data before splitting. Defaults to True.
 
@@ -33,7 +33,10 @@ def split_dataset(X: np.ndarray, y: np.ndarray, test_size=0.2, shuffle=True, see
     """
     _validate_X_y(X, y)
 
-    conjoined = np.concatenate([X, y.reshape(-1, 1)], axis=1)
+    if y.ndim == 1:
+        conjoined = np.concatenate([X, y.reshape(-1, 1)], axis=1)
+    else:
+        conjoined = np.concatenate([X, y], axis=1)
 
     if shuffle:
         if seed is not None:
@@ -45,11 +48,19 @@ def split_dataset(X: np.ndarray, y: np.ndarray, test_size=0.2, shuffle=True, see
     split_idx = int(X.shape[0] * (1 - test_size))
     conjoined_train = conjoined[:split_idx]
     conjoined_test = conjoined[split_idx:]
+    
+    if y.ndim == 1:
+        X_train = conjoined_train[:, :-1]
+        y_train = conjoined_train[:, -1]
 
-    X_train = conjoined_train[:, :-1]
-    y_train = conjoined_train[:, -1]
+        X_test = conjoined_test[:, :-1]
+        y_test = conjoined_test[:, -1]
+    else:
+        num_outputs = y.shape[1]
+        X_train = conjoined_train[:, :-num_outputs]
+        y_train = conjoined_train[:, -num_outputs:]
 
-    X_test = conjoined_test[:, :-1]
-    y_test = conjoined_test[:, -1]
+        X_test = conjoined_test[:, :-num_outputs]
+        y_test = conjoined_test[:, -num_outputs:]
 
     return X_train, y_train, X_test, y_test
